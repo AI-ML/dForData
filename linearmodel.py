5a4163b7429623ff425142528eb2a671135eb75e
Salil Agarwal <salilagarwal18@gmail.com>
2014-07-02 23:11:53 +0530

    batchGradDes fn is now more useable as API. Takes argument x, y, and rate=0.3

--- linearmodel.py
+++ linearmodel.py
@@ -1,12 +1,13 @@
+#   Author: Salil Agarwal
 #   Date: 1/07/2014
-#   Name: Salil Agarwal
+
 
 import pandas as pd
     
     
 def costFnLeastSq( x, y, wgts ):
     """ Implemented the cost function associated with ordinary least square regression
-        model J()"""
+        model """
     p = x.dot( wgts ) #p := pridicted value for y type(p):=pandas.core.series.Series
     diff = p - y[0]   # type(diff) = pandas.core.series.Series; type(y) = DataFrame
     diffsq = pow( diff, 2 )
@@ -14,14 +15,13 @@ def costFnLeastSq( x, y, wgts ):
     error = summ / ( 2.0 * x.index.size )
     return error
     
-def updateWeights( x, y, wgts ):
+def updateWeights( x, y, wgts, rate=0.3 ):
     """Implemention of one iteration of Batch Gradient Descent. 
        Cool thing in this function to notice is that it didn't make use of any 
        for loops. 
        This shows the philosophy of pandas to not use for loops and think in terms 
        of VECTORS """
        
-    rate = 0.3
     p = x.dot( wgts )
     diff = y[0] - p
     summ = x.T.dot( diff ) # This step is awesome and shows the power of pandas.
@@ -29,23 +29,21 @@ def updateWeights( x, y, wgts ):
     wgts = wgts + val
     return wgts
 
-def batchGradDes( x, y ):
+def batchGradDes( x, y, rate=0.3 ):
     wgts = pd.Series( 1.0, x.columns )
     diff = 1.0
     cost = costFnLeastSq( x, y, wgts )
-    print ( "%f \n" %diff )
     counter = 1
-    while diff > 0.0 :
+    while diff > 0.001 :
         counter += 1
-        wgts = updateWeights( x, y, wgts )
+        wgts = updateWeights( x, y, wgts, rate )
         newcost = costFnLeastSq( x, y, wgts )
-        print ( "%f \n" %diff )
         diff = cost - newcost
         cost = newcost
+    print "Parameters: \n "
     print wgts
-    print counter
-    print "\n"
-    print cost
+    print "\nTotal iterations: %d" % counter
+    print "\nFinal Value of cost function: %f" % cost
     return wgts
 
 

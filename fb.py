36aa9eefd18f9a08e23f8206a7ff0a7c372e8cf7
Salil Agarwal <salilagarwal18@gmail.com>
2014-07-29 19:48:42 +0530

    except for FbPageNode.allComments() fn are working

--- fb.py
+++ fb.py
@@ -3,6 +3,7 @@
 
 import requests
 import pandas as pd
+import os
 
 def get_fb( sub_domain='me', params={} ):
     base_url = 'https://graph.facebook.com/v2.0/'
@@ -17,58 +18,163 @@ def get_node_id( nodename='' ):
     return requests.get( base_url ).json()['id']
 
 
-def get_edges( nodeid=None, edgetype='', nodename=None ):
+def file_path( nodeid, edgetype, project ):
+    """ Assign the filepath created using nodeid and edgetype to path """
+    fname = str(nodeid) + '_' + edgetype + '.json'
+    dirsep = os.path.sep
+    path = os.getcwd() + dirsep + 'data' + dirsep + project + dirsep + fname
+    return path
+    
+    
+def get_edges( nodeid, edgetype, project ):
     """ Pull all the edges of type edgetype of node identified by
         nodeid"""
-    if nodeid == None:
-        return
+    nodeid = str(nodeid)
+    path = file_path( nodeid, edgetype, project )
+    if os.path.isfile( path ):
+        return pd.read_json( path )
     
-    print 'Downloading data.'
+    print 'Downloading data: ' + edgetype
     subdomain = nodeid + '/' + edgetype
     fbresponse = get_fb( subdomain )
+    if fbresponse.has_key('error'):
+        print 'Error occurred'
+        return fbresponse['error']
+
     data_list = fbresponse['data']
     
     params = {}
-    params['limit'] = 25
+    params['limit'] = 50
     while fbresponse.has_key('paging') and \
           fbresponse['paging'].has_key('cursors'):
         params['after'] = fbresponse['paging']['cursors']['after']
         fbresponse = get_fb( subdomain, params )
-        data_list.extend( fbresponse['data'] )
-        print '.'
+        if fbresponse.has_key('data'):
+            data_list.extend( fbresponse['data'] )
+            print '.'
     
     if data_list[-1:] == [[]]:
         data_list.pop()
         
     print '\nData download complete.'
-    if nodename != None:
-        fname = nodename + '_' + edgetype + '.txt'
-    else:
-        fname = nodeid + '_' + edgetype + '.txt'
-    fwrite = open( fname, 'w' )
+    
+    fwrite = open( path, 'w' )
     
     data_list.reverse() # .reverse() returns None
     df = pd.DataFrame( data_list )
-    df.to_csv( fwrite, sep='\t', index=False, encoding='utf8' )
+    df.to_json( fwrite )
+    fwrite.close()
     return df
 
+
 class FbNode():
-    def __init__( self, nodename ):
-        self.name_ = nodename
-        self.id_ = get_node_id( self.name_ )
-    def allLikes()
-    def allComments()
-    def allComUsers()
-    def allUsers()
-    def updateNode()
+    def __init__( self, nodeid, projectname ):
+        self.project_ = projectname
+        self.id_ = nodeid
+        
+        
+    def allLikes(self):
+        self.likes_ = get_edges( self.id_, 'likes', self.project_ )
+        return self.likes_
+        
+        
+    def allComments(self):
+        self.comments_ = get_edges( self.id_, 'comments', self.project_ )
+        if self.comments_.columns.size == 0:
+            return self.comments_
+        return self.comments_['message']
+    
+    
+    def allComUsers(self):
+        self.comments_ = get_edges( self.id_, 'comments', self.project_ )
+        if self.comments_.columns.size == 0:
+            return self.comments_
+            
+        return self.comments_['from']
+       
+       
+    def allUsers(self):
+        path = file_path( self.id_, 'user', self.project_ )
+        if os.path.isfile( path ):
+            self.users_ = pd.read_json( path )
+            return self.users_
+            
+        comusers = self.comments_['from'].tolist()#converting a
+                                                # Series of dict to list of dict
+        userslist = comusers.extend(self.likes_.to_dict('record'))
+        self.users_ = pd.DataFrame( userlist )
+        self.users_.to_json( path )
+        return self.users
+        
        
     
-class FbPageNode(FbNode):
+class FbPageNode():
+    def __init__( self, projectname ):
+        self.project_ = projectname
+        self.id_ = get_node_id( projectname )
+        
+        
+    def allLinks( self ):
+        self.links_ = get_edges( self.id_, 'links', self.project_ )
+        return self.links_
     
-    def getAllLinks():
-        return get_edges( self.id_, 'links', self.name_ )
-    def allLinks()
     
+    def allLikes( self ):
+        path = file_path( self.id_, 'likes', self.project_ )
+        if os.path.isfile( path ):
+            print 'its reading from file'
+            self.likes_ = pd.read_json( path )
+            return self.likes_
+            
+        self.links_ = get_edges( self.id_, 'links', self.project_ )
+        linkids = self.links_['id']
+        fwrite = open( path, 'a' )
+        for ids in linkids:
+            linknode = FbNode( ids, self.project_ )
+            linknode.allLikes().to_csv( fwrite, mode='a', sep='\t',\
+                                        index=False, encoding='utf8' )
+        fwrite.close()
+        self.likes_ = pd.read_csv( path, sep='\t', encoding='utf8' )
+        self.likes_.to_json( path )
+        return self.likes_
+    
+    
+    def allComments( self ):
+        path = file_path( self.id_, 'comments', self.project_ )
+        if os.path.isfile( path ):
+            self.comments_ = pd.read_json( path )
+            return self.comments_
+            
+        self.links_ = get_edges( self.id_, 'links', self.project_ )
+        linkids = self.links_['id']
+        fwrite = open( path, 'a' )
+        for ids in linkids:
+            linknode = FbNode( ids, self.project_ )
+            linknode.allComments().to_csv( fwrite, mode='a', sep='\t',\
+                                           index=False, encoding='utf8' )
+        fwrite.close()
+        self.comments_ = pd.read_csv( path,sep='\t', encoding='utf8')
+        self.comments_.to_json( path )
+        return self.comments_ 
+        
+        
+    def allUsers( self ):
+        path = file_path( self.id_, 'users', self.project_ )
+        if os.path.isfile( path ):
+            self.users_ = pd.read_json( path )
+            return self.users_
+        
+        self.links_ = get_edges( self.id_, 'links', self.project_ )
+        linkids = self.links_['id']
+        fwrite = open( path, 'a' )
+        for ids in linkids:
+            linknode = FbNode( ids, self.project_ )
+            linknode.allUsers().to_csv( fwrite, mode='a', sep='\t',\
+                                        index=False, encoding='utf8' )
+        fwrite.close()
+        self.users_ = ( pd.read_csv( path, sep='\t' ,encoding='utf8' ) )
+        self.users_.to_json( path )
+        return self.users_
         
         
         

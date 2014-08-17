52bd44ea5e9e5d6d53a38c184b38ee4980c21f0e
Salil Agarwal <salilagarwal18@gmail.com>
2014-08-18 03:30:04 +0530

    Final

--- odesk/script.py
+++ odesk/script.py
@@ -1,8 +1,12 @@
 #   Author: Salil Agarwal (salil@dfordata.com)
 #   Date:   18/08/2014
 
-from bs4 import BeautifulSoup
-import requests
+try:
+    from bs4 import BeautifulSoup
+    import requests
+except ImportError:
+    print 'You didn\'t have the required packages to run this script'
+
 
 try:
     data = requests.get('http://www.soloboxeo.com/feed/')
@@ -21,8 +25,10 @@ try:
         article = requests.get(url)
         soup1 = BeautifulSoup(article.text)
         content = soup1.find('div',{'id':'postcontent'})
-        if content.table != None:
-            image.append(content.table.img['src'])
+        if content.img != None:
+            image.append(content.img['src'])
+        else:
+            image.append([])
         para = content.find_all('p')
         string = ''
         for i in para:
@@ -31,7 +37,9 @@ try:
 
     print image
     print title
-    print text
+#   print text
     
 except requests.ConnectionError:
     print 'No Internet. Please check your internet connection'
+except NameError:
+    pass
